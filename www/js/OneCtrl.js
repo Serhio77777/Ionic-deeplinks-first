79ec82713b8f36493a36e64c385371bf51b5093e
Serhio77777 <salat_98@mail.ru>
2016-09-01 15:10:28 +0300

    Added pgUtils;

--- www/js/OneCtrl.js
+++ www/js/OneCtrl.js
@@ -1,20 +1,18 @@
 angular.module('app.OneCtrl', ['ionic', 'ionic.native'])
 .controller('OneCtrl', function($scope, $cordovaDeeplinks, $state, $ionicPlatform){
-    $scope.func = function(match) {
-          $cordovaDeeplinks.route({'/account':{ target:'tab-account', parent:'tab.account' }}).subscribe(function(match) {
-              console.log('Does it work?', $cordovaDeeplinks.subscribe);
-              alert('Success!')
-              $timeout(function() {
-                  $state.go(match.$route.parent, match.$args);
-                  $timeout(function() {
-                      $state.go(match.$route.target, match.$args);
-                      alert('Success!');
-                  }, 800);
-              }, 100);
-          }, function(nomatch) {
-            alert('Something is wrong...');
-            console.warn('No match', nomatch);
-          });
-          console.log('It works!');
+    $scope.func = function(appId) {
+          if (window.device && navigator.PGUtils) {
+              navigator.PGUtils.openApp(appId, function(status){
+                // if(status === 'OK') {
+                //     alert('Success!');
+                // } else {
+                //     alert('There is no such app...');
+                // }
+              },
+              function(error) {
+                  alert('Error!');
+                  console.log(error);
+              });
+          }
       };
 })
